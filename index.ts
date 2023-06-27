c829930bbd18859d1a64f0c84e67300dedfb6b07
valentin-gavela <valentin.gavela@despegar.com>
2023-06-27 18:04:33 -0300

    1 file modified, 1 added

--- index.ts
+++ index.ts
@@ -1,5 +1,17 @@
+import { sumZero, sumZeroWithPointers } from './sumZero';
 import { validAnagram } from './validAnagram';
 
+console.log('validAnagram');
 console.log(validAnagram('aca', 'aac'));
 console.log(validAnagram('eepp', 'pepe'));
 console.log(validAnagram('acas', 'aac'));
+
+console.log('sumZero');
+console.log(sumZero([-10, -3, -1, -1, -1, 1, 2, 3, 4, 10]));
+console.log(sumZero([-3, -1, -1, -1, 1, 2, 3, 4]));
+console.log(sumZero([-1, -1, -1, 2, 3, 4]));
+
+console.log('sumZeroWithPointers');
+console.log(sumZeroWithPointers([-10, -3, -1, -1, -1, 1, 2, 3, 4, 10]));
+console.log(sumZeroWithPointers([-3, -1, -1, -1, 1, 2, 3, 4]));
+console.log(sumZeroWithPointers([-1, -1, -1, 2, 3, 4]));
---
+++ sumZero.ts
@@ -0,0 +1,45 @@
+/**Write a fn called sumZero wich accepts a sorted array of integers. The function should find the first pair where the sum is 0.
+ * Return an array that includes both values that sum to zero or undefined if pair does not exist.
+ */
+
+export function sumZero(arr: number[]) {
+  // store in a lookoup obj
+  // loop the arr
+  // find the pair in the lookup obj
+  const objNums = {};
+
+  for (let num of arr) {
+    objNums[num] = null;
+  }
+
+  for (let num of arr) {
+    const counterPart = num * -1;
+
+    if (Object.hasOwn(objNums, counterPart)) {
+      return [Number(num), counterPart];
+    }
+  }
+
+  return undefined;
+}
+
+export function sumZeroWithPointers(arr: number[]) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    const a = arr[left];
+    const b = arr[right];
+    const sum = a + b;
+
+    if (sum === 0) return [a, b];
+
+    if (sum > 0) {
+      right--;
+    } else {
+      left--;
+    }
+  }
+
+  return undefined;
+}
